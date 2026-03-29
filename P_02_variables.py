09b07b4aa32cd27002430c73c9c6b37e06e663d3
wanl50286-cloud <wanl50286@masseyhigh.school.nz>
2026-03-30 10:22:35 +1300

    Variable Basics

---
+++ P_02_variables.py
@@ -0,0 +1,13 @@
+# code to learn about variables...
+
+#Variables can be a string (note the speech marks)
+greeting = "hello world"
+
+# variables can be a number
+fav_num = 7
+num_string = "5"
+
+# Constants are uppercase
+PI = 3.14
+
+a_01_my_num = 7
